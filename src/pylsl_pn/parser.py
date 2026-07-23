fdbc0d5ef431490664c85752ab1ed1a05803330a
Jamie Forth <j.forth@gold.ac.uk>
2026-07-23 19:40:57 +0100

    fix parsing of protocol version and avatar index

--- src/pylsl_pn/parser.py
+++ src/pylsl_pn/parser.py
@@ -2,16 +2,11 @@ import struct
 
 import numpy as np
 
-header_token_format = ">H"
-header_start_token = 0xffdd
-header_end_token = 0xffee
-header_format = "<IH??HH32sII8s"
-
-
-def verify_header(data):
-    start_token = struct.unpack_from(header_token_format, data)[0]
-    end_token = struct.unpack_from(header_token_format, data, 62)[0]
-    return (start_token == header_start_token) & (end_token == header_end_token)
+header_token_format = "<H"
+header_start_token = 0xddff
+header_end_token = 0xeeff
+protocol_format = "<BBBB"
+header_format = "<H??I32sIIII"
 
 
 def valid_packet(data):
@@ -20,27 +15,40 @@ def valid_packet(data):
     return verify_header(data)
 
 
-def parse_header(data):
-    header = struct.unpack_from(header_format, data, 2)
-    header_fields = {
-        "protocol": header[0],
-        "count": header[1],
-        "with_disp": header[2],
-        "with_ref": header[3],
-        # Reserved block?: header[4],
-        "avatar_index": header[5],
-        "avatar_name": header[6].split(b"\x00")[0].decode("utf-8"),
-        "frame_index": header[7],
-        "data_type": header[8],  # Rotation order?
-        # Reserved block?: header[9],
-    }
-    return header_fields
+def verify_header(data):
+    start_token = struct.unpack_from(header_token_format, data)[0]
+    end_token = struct.unpack_from(header_token_format, data, 62)[0]
+    return (start_token == header_start_token) & (end_token == header_end_token)
 
-
-def parse_motion(data, count):
-    return np.frombuffer(data, np.float32, count, 64)
 
 def parse_data(data):
     header = parse_header(data)
     motion = parse_motion(data, header["count"])
     return header, motion
+
+
+def parse_header(data):
+    protocol_version = parse_version(data)
+    header = struct.unpack_from(header_format, data, 6)
+    header_fields = {
+        "protocol": protocol_version,
+        "count": header[0],
+        "with_disp": header[1],
+        "with_ref": header[2],
+        "avatar_index": header[3],
+        "avatar_name": header[4].split(b"\x00")[0].decode("utf-8"),
+        "frame_index": header[5],
+        "reserved_0": header[6],  # Rotation order?
+        "reserved_1": header[7],
+        "reserved_2": header[8],
+    }
+    return header_fields
+
+
+def parse_version(data):
+    build, revision, minor, major = struct.unpack_from(protocol_format, data, 2)
+    return f"{major}.{minor}.{revision}.{build}"
+
+
+def parse_motion(data, count):
+    return np.frombuffer(data, np.float32, count, 64)
--- test/test_binary_parser.py
+++ test/test_binary_parser.py
@@ -7,9 +7,9 @@ import numpy as np
 def test_verify_header():
     header = bytearray(64)
     # Set start token.
-    header[:2] = 0xffdd.to_bytes(2, "big")
+    header[:2] = 0xddff.to_bytes(2, "little")
     # Set end token.
-    header[-2:] = 0xffee.to_bytes(2, "big")
+    header[-2:] = 0xeeff.to_bytes(2, "little")
     assert parser.verify_header(header)
 
 
@@ -18,14 +18,16 @@ def test_parse_header():
         # Read header.
         data = file.read(64)
         header = parser.parse_header(data)
-        assert header["protocol"] == 16842752
+        assert header["protocol"] == "1.1.0.0"
         assert header["count"] == 354
-        assert header["with_disp"] == 1
-        assert header["with_ref"] == 0
+        assert header["with_disp"] is True
+        assert header["with_ref"] is False
         assert header["avatar_index"] == 0
         assert header["avatar_name"] == "RED"
         assert header["frame_index"] == 54
-        assert header["data_type"] == 1
+        assert header["reserved_0"] == 1
+        assert header["reserved_1"] == 0
+        assert header["reserved_2"] == 0
 
 
 def test_parse_data():
@@ -38,14 +40,16 @@ def test_parse_data():
         file.seek(-64, os.SEEK_CUR)
         data = file.read(64 + count * 4)
         header, motion = parser.parse_data(data)
-        assert header["protocol"] == 16842752
+        assert header["protocol"] == "1.1.0.0"
         assert header["count"] == 354
-        assert header["with_disp"] == 1
-        assert header["with_ref"] == 0
+        assert header["with_disp"] is True
+        assert header["with_ref"] is False
         assert header["avatar_index"] == 0
         assert header["avatar_name"] == "RED"
         assert header["frame_index"] == 54
-        assert header["data_type"] == 1
+        assert header["reserved_0"] == 1
+        assert header["reserved_1"] == 0
+        assert header["reserved_2"] == 0
 
         np.testing.assert_equal(motion[0], -55.208133697509766)
         np.testing.assert_equal(motion[-1], -20)
@@ -58,14 +62,16 @@ def test_parse_data():
         file.seek(-64, os.SEEK_CUR)
         data = file.read(64 + count * 4)
         header, motion = parser.parse_data(data)
-        assert header["protocol"] == 16842752
+        assert header["protocol"] == "1.1.0.0"
         assert header["count"] == 354
-        assert header["with_disp"] == 1
-        assert header["with_ref"] == 0
+        assert header["with_disp"] is True
+        assert header["with_ref"] is False
         assert header["avatar_index"] == 0
         assert header["avatar_name"] == "RED"
         assert header["frame_index"] == 55
-        assert header["data_type"] == 1
+        assert header["reserved_0"] == 1
+        assert header["reserved_1"] == 0
+        assert header["reserved_2"] == 0
 
         np.testing.assert_equal(motion[0], -55.21929168701172)
         np.testing.assert_equal(motion[-1], -20)
